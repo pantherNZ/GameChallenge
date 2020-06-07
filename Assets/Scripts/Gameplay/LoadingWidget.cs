5647c0b81007773650c7d9b56cfcf97046f71446
Alex Denford <pantherNZ@users.noreply.github.com>
2020-06-08 01:26:02 +1200

    Login screen, connecting different screens,

---
+++ Assets/Scripts/Gameplay/LoadingWidget.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingWidget : MonoBehaviour
+{
+    public Image image;
+    public int numCircles = 5;
+    public int numCirclesForSpacing = 5;
+    public float speed = 1.0f;
+    public float scalePerCircle = 1.1f;
+    List<Image> images = new List<Image>();
+    new RectTransform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = base.transform as RectTransform;
+        float scale = 1.0f;
+        for( int i = 0; i < numCircles; ++i )
+        {
+            images.Add( Instantiate( image, transform ) );
+            ( images.Back().transform as RectTransform ).localScale = new Vector3( scale, scale, scale );
+            scale *= scalePerCircle;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for( int i = 0; i < images.Count; ++i )
+        {
+            float angle = Mathf.PI * 2.0f / numCirclesForSpacing * i + Time.time * speed;
+            var offset = new Vector3( Mathf.Cos( angle ), Mathf.Sin( angle ), transform.position.z );
+            ( images[i].transform as RectTransform ).anchoredPosition = transform.position + offset * transform.rect.width / 2.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/LoginUI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginUI : MonoBehaviour
+{
+    public Button loginButton = null;
+    public InputField passworldInput = null;
+    public CanvasGroup loginDisplay = null;
+    public CanvasGroup incorrectPasswordDisplay = null;
+    public CanvasGroup desktopUI = null;
+    public Button incorrectPasswordButton = null;
+    string correctPasswordHash = "$HASH|V1$10000$r53uOrQ3a+oqPR+jGmdpDxnP+vJflbIresLp0ji7uBmRrp9/";
+
+    private void Start()
+    {
+        loginButton.onClick.AddListener( () =>
+        {
+            if( passworldInput.text.Length > 0 && SecurePasswordHasher.Verify( passworldInput.text, correctPasswordHash ) )
+            {
+                GetComponent<CanvasGroup>().ToggleVisibility();
+                desktopUI.ToggleVisibility();
+            }
+            else
+            {
+                loginDisplay.ToggleVisibility();
+                incorrectPasswordDisplay.ToggleVisibility();
+                passworldInput.text = string.Empty;
+            }
+        } );
+
+        incorrectPasswordButton.onClick.AddListener( () =>
+        {
+            loginDisplay.ToggleVisibility();
+            incorrectPasswordDisplay.ToggleVisibility();
+        } );
+    }
+
+    private void Update()
+    {
+        if( Input.GetKeyUp( KeyCode.Return ) )
+        {
+            loginButton.onClick.Invoke();
+        }
+    }
+}
