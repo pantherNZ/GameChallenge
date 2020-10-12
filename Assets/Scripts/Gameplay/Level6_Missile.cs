e5d317d191f0e86524580af2614f783d273f12b5
Alex Denford <pantherNZ@users.noreply.github.com>
2020-10-13 01:27:16 +1300

    Missile firing setup + animations.

--- Assets/Scripts/Gameplay/Level6_Missile.cs
+++ Assets/Scripts/Gameplay/Level6_Missile.cs
@@ -7,8 +7,13 @@ using UnityEngine;
 
 public class Level6_Missile : BaseLevel
 {
+    [SerializeField] GameObject missileLauncherPrefab = null;
+    [SerializeField] GameObject missilePrefab = null;
+    [SerializeField] float moveDist = 20.0f;
+    [SerializeField] float moveSpeed = 10.0f;
     List<GameObject> windows = new List<GameObject>();
-    GameObject shortcut;
+    GameObject shortcut, missileLauncher;
+    List<GameObject> missiles = new List<GameObject>();
 
     public override void OnStartLevel()
     {
@@ -19,6 +24,12 @@ public class Level6_Missile : BaseLevel
             icon = Resources.Load<Texture2D>( "Textures/Full_Recycle_Bin" )
         };
         shortcut = desktop.CreateShortcut( icon, new Vector2Int( 0, 1 ), ( x ) => windows.Add( desktop.CreateWindow( "Missiles" ) ) );
+
+        missileLauncher = Instantiate( missileLauncherPrefab, windows.Back().GetComponent<Window>().windowCamera.gameObject.transform );
+        missileLauncher.transform.localPosition = new Vector3( -3.5f, 0.0f, 50.0f );
+        missileLauncher.transform.localEulerAngles = new Vector3( 0.0f, 0.0f, -90.0f );
+
+        Utility.FunctionTimer.CreateTimer( 3.0f, FireMissile, "FireMissile", true );
     }
 
     protected override void OnLevelUpdate()
@@ -26,4 +37,33 @@ public class Level6_Missile : BaseLevel
         base.OnLevelUpdate();
     }
 
+    void FireMissile()
+    {
+        missileLauncher.GetComponent<Animator>().Play( "Fire" );
+
+        Utility.FunctionTimer.CreateTimer( 0.5f, () =>
+        {
+            var missile = Instantiate( missilePrefab );
+            var spawnLocation = missileLauncher.GetComponentInChildren<Transform>();
+            missile.transform.position = spawnLocation.position;
+            missile.transform.rotation = spawnLocation.rotation;
+            StartCoroutine( MoveMissileRoutine( missile ) );
+            missiles.Add( missile );
+        } );
+    }
+
+    public System.Collections.IEnumerator MoveMissileRoutine( GameObject missile )
+    {
+        yield return StartCoroutine( Utility.InterpolatePosition( missile.transform, missile.transform.position + new Vector3( moveDist, 0.0f, 0.0f ), moveDist / moveSpeed ) );
+        missile.GetComponent<Animator>().Play( "Explode" );
+
+        Utility.FunctionTimer.CreateTimer( 1.0f, () =>
+        {
+            if( missile != null )
+            {
+                missiles.Remove( missile );
+                missile.Destroy();
+            }
+        } );
+    }
 }
