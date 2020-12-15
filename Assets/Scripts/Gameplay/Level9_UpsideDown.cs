8317d9c5b21ec7639c05ca8c39c7efdfa8d89106
Alex Denford <pantherNZ@users.noreply.github.com>
2020-12-16 01:10:57 +1300

    Stuff

--- Assets/Scripts/Gameplay/Level9_UpsideDown.cs
+++ Assets/Scripts/Gameplay/Level9_UpsideDown.cs
@@ -18,23 +18,30 @@ public class Level9_UpsideDown : BaseLevel
         desktop.DesktopCanvas.localRotation = Quaternion.Euler( 0.0f, 0.0f, 180.0f );
         subtitlesCanvas.localRotation = Quaternion.Euler( 0.0f, 0.0f, 180.0f );
         timeGap = TimeSpan.FromMinutes( 37 );
+        Reset();
 
         timeSetter.OnTimeChangedEvent += ( _, oldT, newT ) => 
         {
             var targetTime = DateTime.Now.TimeOfDay.Add( timeGap );
-            var rotation = ( ( timeSetter.Time - targetTime ).Minutes / ( float )timeGap.Minutes ) * 180.0f;
-            desktop.DesktopCanvas.localRotation = Quaternion.Euler( 0.0f, 0.0f, rotation );
-            subtitlesCanvas.localRotation = Quaternion.Euler( 0.0f, 0.0f, rotation );
+            var rotation = ( ( targetTime - timeSetter.Time ).TotalSeconds / timeGap.TotalSeconds ) * 180.0f;
+            desktop.DesktopCanvas.localRotation = Quaternion.Euler( 0.0f, 0.0f, ( float )rotation );
+            subtitlesCanvas.localRotation = Quaternion.Euler( 0.0f, 0.0f, ( float )rotation );
         };
 
         SubtitlesManager.Instance.AddSubtitleGameString( "Narrator_Level_9_1" );
     }
 
-    private void Update()
+    public void Reset()
+    {
+        currentTime = DateTime.Now.TimeOfDay;
+        timeSetter.Time = currentTime;
+    }
+
+    override protected void OnLevelUpdate()
     {
         var newTime = DateTime.Now.TimeOfDay;
 
-        if( newTime.Minutes != currentTime.Minutes )
+        if( newTime.Seconds != currentTime.Seconds )
         {
             var oldTime = currentTime;
             currentTime = newTime;
